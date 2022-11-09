06603bb5c3e1a3f97dfd36c60d551fd3c41cf7d6
Laura Beutler <labeutler@gmail.com>
2022-11-08 19:06:08 -0500

    adding api for accuweather to work.  Added addtional containers for previous searches.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,30 @@
+
+// Past seach storage information
+var pastSearches = [];
+
+if(localStorage["pastSearches"]) {
+    pastSearches = JSON.parse(localStorage[pastSearches]);
+}
+
+// Draw past searches
+function drawPastSearches() {
+    if(pastSearches.length) {
+        var html = pastSearchesTemplate({search:pastSearches});
+        $("#pastSearches").html(html);
+    }
+}
+
+$(document).on("click", ".pastSearchLink", function(e) {
+    e.preventDefault();
+    var search = $(this).text();
+    doSearch(search);
+});
+
+// API info for weather forecast
+function weatherApi(){
 const encodedParams = new URLSearchParams();
-encodedParams.append("apiKey", "<REQUIRED>");
-encodedParams.append("locationKey", "<REQUIRED>");
+encodedParams.append("apiKey", "c35a4bee2emsh6c315d3399c090bp11f346jsnc0801ef84967");
+encodedParams.append("locationKey", "city");
 
 const options = {
 	method: 'POST',
@@ -16,3 +40,4 @@ fetch('https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsB
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+}
