1a8fa5908d47e13f53ee3f13a51882f2a3e12eb0
Laura Beutler <labeutler@gmail.com>
2022-11-09 15:10:17 -0500

    adding form information to assist with collecting data

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,55 @@
+var city = []
+
+// City Search form
+var cityForm = function (event) {
+	event.preventDefault();
+	var city = inputbox.value.trim();
+	if (city) {
+		getWeather(city);
+		get5Day(city);
+		citites.unshift({ city });
+		inputbox.value = "";
+	} else {
+		alert("Required: Name of City");
+	}
+	saveSearch();
+	pastSearches(city);
+	console.log(city)
+}
 
 // Past seach storage information
 var pastSearches = [];
 
-if(localStorage["pastSearches"]) {
-    pastSearches = JSON.parse(localStorage[pastSearches]);
+if (localStorage["pastSearches"]) {
+	pastSearches = JSON.parse(localStorage[pastSearches]);
 }
 
 // Draw past searches
 function drawPastSearches() {
-    if(pastSearches.length) {
-        var html = pastSearchesTemplate({search:pastSearches});
-        $("#pastSearches").html(html);
-    }
+	if (pastSearches.length) {
+		var html = pastSearchesTemplate({ search: pastSearches });
+		$("#pastSearches").html(html);
+	}
 }
 
-$(document).on("click", ".pastSearchLink", function(e) {
-    e.preventDefault();
-    var search = $(this).text();
-    doSearch(search);
+$(document).on("click", ".pastSearchLink", function (e) {
+	e.preventDefault();
+	var search = $(this).text();
+	doSearch(search);
 });
 
 // API info for weather forecast
-function weatherApi(){
-const encodedParams = new URLSearchParams();
-encodedParams.append("apiKey", "c35a4bee2emsh6c315d3399c090bp11f346jsnc0801ef84967");
-encodedParams.append("locationKey", "city");
+var getWeather = function (weatherApi) {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'c35a4bee2emsh6c315d3399c090bp11f346jsnc0801ef84967',
+			'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
+		}
+	};
 
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'c35a4bee2emsh6c315d3399c090bp11f346jsnc0801ef84967',
-		'X-RapidAPI-Host': 'AccuWeatherstefan-skliarovV1.p.rapidapi.com'
-	},
-	body: encodedParams
-};
-
-fetch('https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	fetch('https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D?units=auto&lang=en', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
 }
